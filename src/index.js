f6142976691901027d98f4961e47e4ca0c90905e
geeky-amit <43772459+geeky-amit@users.noreply.github.com>
2022-11-02 20:52:23 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,42 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-const userRouter = require('./routes/userRoutes');
-const noteRouter = require('./routes/noteRoutes');
+const userRouter = require("./routes/userRoutes");
+const noteRouter = require("./routes/noteRoutes");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
-dotenv.config();
+dotenv.config({ path: __dirname + "/.env" });
 
 app.use(express.json());
 
 app.use(cors());
 
-
-
 app.use("/users", userRouter);
 app.use("/note", noteRouter);
 
-
-app.get('/', (req, res) => {
-                res.send("Notes API from COCO");
+app.get("/", (req, res) => {
+  res.send("Notes API from COCO");
 });
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect("mongodb+srv://coco:Cocosh_02@cluster0.akpuf4f.mongodb.net/notes_db?retryWrites=true&w=majority")
-.then(() => {
-
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
     app.listen(PORT, () => {
-        console.log("Server started on port  no. " + PORT);
+      console.log("Server started on port  no. " + PORT);
     });
-
-})
-.catch((error) => {
+  })
+  .catch((error) => {
     console.log(error);
-})
-
-
+  });
